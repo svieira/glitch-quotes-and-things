f38497534fa3e29d3373a967fe250f61e72c7257
System <none>
2017-04-04 15:47:03 +0000

    Checkpoint

--- handers/expressions.js
+++ handers/expressions.js
@@ -1,5 +1,19 @@
 const fetch = require('node-fetch');
 
 module.exports = function expressionHandler(request, response) {
-  
+  const expression = request.body.text;
+  fetch('https://cy175v8jxb.execute-api.us-east-1.amazonaws.com/evaluatorPrototype/', {
+    method: 'POST',
+    body: expression
+  }).then(res => res.json()).then(body => {
+    response.json({
+      response_type: 'in_channel',
+      body
+    })
+  }).catch(err => {
+    response.json({
+      response_type: 'in_channel',
+      text: err.toString()
+    });
+  })
 };
