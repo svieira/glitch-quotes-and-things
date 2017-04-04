a5ad25e1c475b765ff078b6e7fd3ff48dd34cef3
System <none>
2017-04-04 15:36:57 +0000

    Checkpoint

---
+++ handers/expressions.js
@@ -0,0 +1,5 @@
+const fetch = require('node-fetch');
+
+module.exports = function expressionHandler(request, response) {
+  
+};
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const formMiddleware = require('body-parser');
-const Color = require('color');
+const colorHandler = require('./handlers/color');
 const express = require('express');
 
 const app = express();
@@ -7,92 +7,6 @@ app.use(
   formMiddleware.json(),
   formMiddleware.urlencoded({ extended: true })
 );
-
-const randomRgbComponent = () => Math.floor(Math.random() * 255);
-
-const randomColor = () => Color({
-  r: randomRgbComponent(),
-  g: randomRgbComponent(),
-  b: randomRgbComponent()
-});
-
-function getColor(color) {
-  if (!color) return randomColor();
-  try {
-    return Color(color);
-  } catch (e) {
-    console.log('Unable to create a color for', color, 'due to', e);
-    return randomColor();
-  }
-}
-
-const WHITE = Color('#fff'), BLACK = Color('#000');
-const SIZES = {
-  Large: {
-    width: 64, height: 64
-  },
-  Small: {
-    width: 16, height: 16
-  }
-};
-
-const colorStringLike = /(#[0-9a-f]{3,8}|(rgba?|hsla?|hwba?)\([0-9.,%\s]+\))/ig
-
-const isSingleColor = s => {
-  try { Color(s); return true; } catch (e) { return false; }
-};
-
-const template = ({color, size = SIZES.Large}) => {
-  const inverseColor = color.light() ? BLACK : WHITE;
-  const text = size === SIZES.Small ? '' : `
-<text
-    x="50%" y="50%" alignment-baseline="middle" text-anchor="middle"
-    style="font-family: monospace; font-weight: bold; fill: ${inverseColor.hex()};">${color.hex()}</text>
-`.trim();
-  return `
-<svg xmlns="http://www.w3.org/2000/svg"
-     width="${size.width}" height="${size.height}" viewBox="0 0 100 100">
-  <rect x="0" y="0" height="100" width="100"
-          style="fill: ${color.hex().toLowerCase()};"/>
-  ${text}
-</svg>
-`.trim();
-}
-
-const mdTemplate = (svg, alt='') => `![${alt}](${toDataUri(svg)})`;
-
-const toDataUri = svg => 'data:image/svg+xml;base64,' + new Buffer(svg).toString('base64');
-
-function standAloneColorResponse(color) {
-  color = getColor(color);
-  const svg = template({color});
-  return mdTemplate(svg, `The color ${color.hex()}`);
-}
-
-function multipleColorResponse(str) {
-  const toMd = match => {
-    try {
-      const color = Color(match);
-      return mdTemplate(template({color, size: SIZES.Small})) + ' ' + match;
-    } catch (e) {
-      return match;
-    }
-  };
-  return str.replace(colorStringLike, toMd).replace(/\b[a-z]+\b/g, toMd);
-}
-
-
-function colorHandler(request, response) {
-  var color = (request.body && request.body.text || '');
-  console.log('Got a request with', color);
-
-  const text = isSingleColor(color) ? standAloneColorResponse(color) : multipleColorResponse(color);
-  console.log(text);
-  response.json({
-    response_type: 'in_channel',
-    text
-  });
-}
 
 app.get('/color', colorHandler);
 app.post('/color', colorHandler);
