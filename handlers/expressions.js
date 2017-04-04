8a95a975acd027b402d72a2ce930a255195a413b
System <none>
2017-04-04 16:37:28 +0000

    Checkpoint

--- handlers/expressions.js
+++ handlers/expressions.js
@@ -5,15 +5,18 @@ const codeBlock = (code, language='') => '```${language}\n' + code + '\n```'
 const template = ({expression, result, type}) => {
   return `
 ${codeBlock(expression, 'groovy')}
-${}
+${'`--->`'}
 ${codeBlock(result)}
 ###### ${type}
 `.trim();
 }
+const quietFlag = /\b-?-(quiet|q|s|silent|me)\b/ig;
+const isQuiet = s => quietFlag.test(s)
 
 module.exports = function expressionHandler(request, response) {
-  const expression = request.body.text;
-  console.log('Received', expression)
+  const quiet = isQuiet(request.body.text);
+  const expression = (quiet ? request.body.text.replace(quietFlag, '') : request.body.text).trim();
+  const response_type = quiet ? 'ephemeral' : 'in_channel';
   fetch('https://cy175v8jxb.execute-api.us-east-1.amazonaws.com/evaluatorPrototype/', {
     method: 'POST',
     headers: {
@@ -23,12 +26,12 @@ module.exports = function expressionHandler(request, response) {
     body: JSON.stringify(expression)
   }).then(res => res.json()).then(body => {
     response.json({
-      response_type: 'in_channel',
+      response_type,
       text: template({expression, type: body.type, result: body.value})
     })
   }).catch(err => {
     response.json({
-      response_type: 'in_channel',
+      response_type,
       text: template({expression, type: 'Error', result: err})
     });
   })
