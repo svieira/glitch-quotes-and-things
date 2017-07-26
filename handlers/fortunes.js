4b0ebcfe569be2c9f6f8120e72d5d92f77ef4e09
System <none>
2017-07-26 03:49:23 +0000

    Checkpoint

--- handlers/fortunes.js
+++ handlers/fortunes.js
@@ -3,11 +3,18 @@ const FORTUNES = `
 The society which scorns excellence in plumbing as a humble activity and tolerates shoddiness in philosophy because it is an exaulted activity will have neither good plumbing nor good philosophy ... neither its pipes nor its theories will hold water.
 `.split(/\n%\n/g).map(t => t.trim()).filter(s => !!s).map(s => s.indexOf(">") === 0 ? s : '>' + s.split('\n').join('\n>'));
 
+const ICONS = `
+https://cdn.glitch.com/8568201b-555b-4c6e-8e58-9e525d75d1d7%2Fimage.png?1501040026835
+https://cdn.glitch.com/8568201b-555b-4c6e-8e58-9e525d75d1d7%2Fcookie.png?1501040330593
+https://cdn.glitch.com/8568201b-555b-4c6e-8e58-9e525d75d1d7%2Fa-mirror.png?1501040717131
+`.trim().split('\n')
+
 module.exports = function fortuneHandler(request, response) {
   const text = FORTUNES[Math.floor(Math.random() * FORTUNES.length)];
+  const icon_url = ICONS[Math.floor(Math.random() * ICONS.length)];
   return response.json({
     response_type: 'in_channel',
     text,
-    icon_url: 'https://cdn.glitch.com/8568201b-555b-4c6e-8e58-9e525d75d1d7%2Fimage.png?1501040026835'
+    icon_url
   });
 }
