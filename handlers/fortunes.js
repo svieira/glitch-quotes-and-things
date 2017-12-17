ec2a7dc31cd1f2f7523900b8cd866af629567d04
Glitch (tree-map) <none>
2017-12-17 02:39:51 +0000

    🦄🎺 Checkpoint handlers/fortunes.js:286779 package.json:286779

--- handlers/fortunes.js
+++ handlers/fortunes.js
@@ -255,6 +255,8 @@ No battle was ever won according to plan, but no battle was ever won without one
 %
 In the matter of reforming things, as distinct from deforming them, there is one plain and simple principle; a principle which will probably be called a paradox. There exists in such a case a certain institution or law; let us say, for the sake of simplicity, a fence or gate erected across a road. The more modern type of reformer goes gaily up to it and says, "I don't see the use of this; let us clear it away." To which the more intelligent type of reformer will do well to answer: "If you don't see the use of it, I certainly won't let you clear it away. Go away and think. Then, when you can come back and tell me that you do see the use of it, I may allow you to destroy it."
 ~ G. K. Chesterton
+%
+The iron law of bureaucracy states that for all organizations, most of their activity will be devoted to the perpetuation of the organization, not to the pursuit of its ostensible objective.
 `.split(/\n%\n/g).map(t => t.trim()).filter(s => !!s).map(s => s.indexOf(">") === 0 ? s : '>' + s.split('\n').join('\n>'));
 
 const ICONS = `
