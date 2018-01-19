25c0c75f494021a6de5bd5074b50a8eaffe08538
Glitch (tree-map) <none>
2018-01-19 02:50:12 +0000

    🏓🚣 Checkpoint handlers/with-help.js:286779/21 handlers/jspaint.js:286779/6 handlers/appear-in.js:286779/27 handlers/random-name.js:286779/118

---
+++ handlers/jspaint.js
@@ -0,0 +1,1 @@
+"http://jspaint.ml/#session:templated-name"
---
+++ handlers/random-name.js
@@ -0,0 +1,86 @@
+const NOUNS = `
+rabbit
+hat
+cat
+bird
+ship
+sail
+float
+boat
+chariot
+horse
+flute
+prune
+apple
+grape
+tuna
+salmon
+pretzel
+king
+queen
+jack
+ace
+shovel
+trowel
+moth
+sand
+shell
+corn
+llama
+alpaca
+sheep
+goat
+goldfish
+canary
+silver
+gold
+ruby
+diamond
+finch
+python
+bucket
+coffee
+tea
+tree
+brook
+`.trim().split('\n');
+const ADJECTIVES = `
+kind
+truthful
+running
+floating
+singing
+happy
+merry
+golden
+silvered
+ancient
+young
+tame
+fierce
+trim
+red
+green
+blue
+silver
+gold
+umber
+ocher
+sweet
+ruby
+diamond
+`.trim().split('\n');
+
+module.exports = function randomName(size = 4, { NOUNS = NOUNS, ADJECTIVES = ADJECTIVES } = {}) {
+  let result = [];
+  let adjective = true;
+  while (result.length < size) {
+    const words = adjective ? ADJECTIVES : NOUNS;
+    const wordsLength = words.length;
+    const index = Math.floor(Math.random() * wordsLength)
+    if (index === wordsLength || result.includes(words[index])) continue;
+    result.push(words[index]);
+    adjective = !adjective;
+  }
+  return result.join('-');
+};
