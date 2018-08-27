e2ffa1b95c6664f923d209fce71c8f7f9f97024e
Glitch (tree-map) <none>
2018-08-27 18:04:28 +0000

    ⛲️🎎 Checkpoint ./handlers/fortunes-data.js:286779/1781

--- handlers/fortunes-data.js
+++ handlers/fortunes-data.js
@@ -343,6 +343,16 @@ What’s the best thing about telling UDP jokes?
 I don’t have to care if you get them.
 %
 If geometry were as much opposed to our passions and present interests as is ethics, we should contest it and violate it but little less, notwithstanding all the demonstrations of Euclid and Archimedes
+%
+I've made a conscious effort to avoid web development in the past five or so years, because it seems like the Hamster Wheel of Backwards Incompatibility has become more of a Hamster Centrifuge in that field.
+%
+In my life as an architect, I find that the single thing which inhibits young professionals, new students most severely, is their acceptance of standards that are too low. If I ask a student whether her design is as good as Chartres, she often smiles tolerantly at me as if to say, "Of course not, that isn’t what I am trying to do. . . . I could never do that."  
+
+Then, I express my disagreement, and tell her: "That standard must be our standard. If you are going to be a builder, no other standard is worthwhile. That is what I expect of myself in my own buildings, and it is what I expect of my students." Gradually, I show the students that they have a right to ask this of themselves, and must ask this of themselves. Once that level of standard is in their minds, they will be able to figure out, for themselves, how to do better, how to make something that is as profound as that.  
+
+Two things emanate from this changed standard. First, the work becomes more fun. It is deeper, it never gets tiresome or boring, because one can never really attain this standard. One's work becomes a lifelong work, and one keeps trying and trying. So it becomes very fulfilling, to live in the light of a goal like this.  
+
+But secondly, it does change what people are trying to do. It takes away from them the everyday, lower-level aspiration that is purely technical in nature, (and which we have come to accept) and replaces it with something deep, which will make a real difference to all of us that inhabit the earth.
 `.split(/\n%\n/g).map(t => t.trim()).filter(s => !!s).map(s => s.indexOf(">") === 0 ? s : '>' + s.split('\n').join('\n>'));
 
 const ICONS = `
