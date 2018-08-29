4a0542212f39a9b78bd8b4ac4c1d7f96263703b8
Glitch (tree-map) <none>
2018-08-29 20:01:19 +0000

    🏘🎍 Checkpoint ./handlers/appear-in.js:286779/30

--- handlers/appear-in.js
+++ handlers/appear-in.js
@@ -24,10 +24,10 @@ If a longer message is provided, the bot echos that message with a random room n
 
 module.exports = withHelp({text: HELP}, function appearInHandler(request, response) {
   const message = (request.body && request.body.text || '').trim();
-  const isSingleWord = /^[\w\d_.-]+$/i.test(message);
+  const isSingleWord = /^[\w\d_.-]*$/i.test(message);
   const roomName = (isSingleWord && message || randomName());
   const text = isSingleWord 
-    ? `[Please join me in appear.in/${roomName}](https://appear.in/${roomName})`
+    ? `[Please join @${request.body.user_name} in appear.in/${roomName}](https://appear.in/${roomName})`
     : `${message}\n[appear.in/${roomName}](https://appear.in/${roomName})`
 
   response.json({
