bd5e46ffc2af70627c978a050d396c50d0788694
Glitch (tree-map) <none>
2019-02-23 17:43:43 +0000

    🚥👖 Checkpoint ./server.js:286779/215

--- server.js
+++ server.js
@@ -26,6 +26,21 @@ app.post('/expression', expressionHandler);
 app.post('/appear-in', appearInHandler);
 app.post('/fortune', fortuneHandler);
 
+app.get('/', (request, response) => {
+  response.html(`
+<!doctype html>
+<html>
+<head>
+<title>Random Bots</title>
+</head>
+<body>
+<h1>Test the bots here</h1>
+
+</body>
+</html>
+`.trim());
+});
+
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
