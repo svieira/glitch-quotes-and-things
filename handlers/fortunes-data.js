963ad115e7e138eecc789611a96ae152229eb5e7
Glitch (tree-map) <none>
2019-08-27 02:54:04 +0000

    🎗🐪 Checkpoint ./handlers/fortunes-data.js:286779/3400

--- handlers/fortunes-data.js
+++ handlers/fortunes-data.js
@@ -849,6 +849,38 @@ Before my surgery my anaesthetist offered to knock me out with gas or a boat pad
 %
 Photos do not lie, it is said. But frequently they tell partial truths. They tear moments out of time's flowing river and separate them from what has come before and what will follow. They can misrepresent as well as represent.
 %
+WARNING: This product warps space and time in its vicinity.
+%
+WARNING: This product attracts every other piece of matter in the universe, including the products of other manufacturers, with a force proportional to the product of the masses and inversely proportional to the distance between them.
+%
+CAUTION: The mass of this product contains the energy equivalent of 85 million tons of TNT per net ounce of weight.
+%
+HANDLE WITH EXTREME CARE: This product contains minute electrically charged particles moving at velocities in excess of five hundred million miles per hour.
+%
+CONSUMER NOTICE: Because of the "uncertainty principle," it is impossible for the consumer to find out at the same time both precisely where this product is and how fast it is moving.
+%
+ADVISORY: There is an extremely small but nonzero chance that, through a process known as "tunneling," this product may spontaneously disappear from its present location and reappear at any random place in the universe, including your neighbor's domicile. The manufacturer will not be responsible for any damages or inconveniences that may result.
+%
+READ THIS BEFORE OPENING PACKAGE: According to certain suggested versions of the Grand Unified Theory, the primary particles constituting this product may decay to nothingness within the next four hundred million years.
+%
+THIS IS A 100% MATTER PRODUCT: In the unlikely event that this merchandise should contact antimatter in any form, a catastrophic explosion will result.
+%
+PUBLIC NOTICE AS REQUIRED BY LAW: Any use of this product, in any manner whatsoever, will increase the amount of disorder in the universe. Although no liability is implied herein, the consumer is warned that this process will ultimately lead to the heat death of the universe.
+%
+NOTE: The most fundamental particles in this product are held together by a "gluing" force about which little is currently known and whose adhesive power can therefore not be permanently guaranteed.
+%
+ATTENTION: Despite any other listing of product contents found hereon, the consumer is advised that, in actuality, this product consists of 99.9999999999% empty space.
+%
+NEW GRAND UNIFIED THEORY DISCLAIMER: The manufacturer may technically be entitled to claim that this product is ten-dimensional. However, the consumer is reminded that this confers no legal rights above and beyond those applicable to three-dimensional objects, since the seven new dimensions are "rolled up" into such a small "area" that they cannot be detected.
+%
+PLEASE NOTE: Some quantum physics theories suggest that when the consumer is not directly observing this product, it may cease to exist or will exist only in a vague and undetermined state.
+%
+COMPONENT EQUIVALENCY NOTICE: The subatomic particles (electrons, protons, etc.) comprising this product are exactly the same in every measurable respect as those used in the products of other manufacturers, and no claim to the contrary may legitimately be expressed or implied.
+%
+HEALTH WARNING: Care should be taken when lifting this product, since its mass, and thus its weight, is dependent on its velocity relative to the user.
+%
+IMPORTANT NOTICE TO PURCHASERS: The entire physical universe, including this product, may one day collapse back into an infinitesimally small space. Should another universe subsequently re-emerge, the existence of this product in that universe cannot be guaranteed.
+%
 `.split(/\n%\n/g).map(t => t.trim()).filter(s => !!s).map(s => s.indexOf(">") === 0 ? s : '>' + s.split('\n').join('\n>'));
 
 // More can be found here: http://quotes.cat-v.org/programming/
